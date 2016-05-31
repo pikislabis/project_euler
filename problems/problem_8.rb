347d2694848aa0a34c177d69083d79d8ceb2b53e
Jose Antonio Parra <japarra@redborder.net>
2016-05-31 12:38:47 +0200

    Remove pry require

--- problems/problem_8.rb
+++ problems/problem_8.rb
@@ -1,7 +1,5 @@
 #!/usr/bin/env ruby
 
-require 'pry'
-
 # The four adjacent digits in the 1000-digit number that have the greatest product are 9 × 9 × 8 × 9 = 5832.
 #
 # 73167176531330624919225119674426574742355349194934
